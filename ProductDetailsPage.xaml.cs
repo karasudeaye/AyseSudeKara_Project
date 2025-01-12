1e9eb893ec22606c40fd4eac7dc335096832aca0
Ayşe Sude Kara <ayse.kara@std.bogazici.edu.tr>
2025-01-12 15:45:12 +0300

    AYSESUDEKARA

--- ProductDetailsPage.xaml.cs
+++ ProductDetailsPage.xaml.cs
@@ -1,9 +1,21 @@
-namespace AyseSudeKara_Project;
+using Microsoft.Maui.Controls;
 
-public partial class ProductDetailsPage : ContentPage
+namespace AyseSudeKara_Project
 {
-	public ProductDetailsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class ProductDetailsPage : ContentPage
+    {
+        
+        public ProductDetailsPage(Product product)
+        {
+            InitializeComponent();
+
+
+            ProductNameLabel.Text = product.Name;
+            ProductImage.Source = product.ImageSource;
+            ProductTypeLabel.Text = $"Tür: {product.Type}";
+            ProductUsageLabel.Text = $"Kullaným Sýklýðý: {product.UsageFrequency}";
+            ProductAdditionalInfoLabel.Text = $"Bilgi: {product.AdditionalInfo}";
+        }
+    }
+
 }
